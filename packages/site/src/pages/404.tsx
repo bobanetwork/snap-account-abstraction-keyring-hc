90b049c058ce13e607f83e9693616b766ebb51c4
sahil kashetwar <sahil@enya.ai>
2025-04-16 18:35:39 +0530

    formated and removed unused code

--- packages/site/src/pages/404.tsx
+++ packages/site/src/pages/404.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import { Card } from '../components'
 import { CardContainer, Container } from '../components/styledComponents'
-import { ConnectButton, Card, ReloadButton } from '../components'
 
 const Page404 = () => {
   return (
     <Container>
       <CardContainer>
-                <Card
-                  content={{
-                    description: 'Something went wrong please reload app',
-                    button: (
-                      <a href="/">Reload Page</a>
-                    ),
-                  }}
-                  // disabled={!state.hasMetaMask}
-                />
-              </CardContainer>
+        <Card
+          content={{
+            description: 'Something went wrong please reload app',
+            button: (
+              <a href="/">Reload Page</a>
+            ),
+          }}
+        // disabled={!state.hasMetaMask}
+        />
+      </CardContainer>
     </Container>
   )
 }
