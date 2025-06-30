f3d5ea6428f3ec47f022ef43a4a23c8136be7bec
sahil kashetwar <sahil@enya.ai>
2025-06-30 17:58:52 +0530

    fix: metamask class moved from types to utils itself

--- packages/site/src/utils/metamask.ts
+++ packages/site/src/utils/metamask.ts
@@ -1,4 +1,14 @@
-import { MetaMaskError } from '../types/metamask';
+export class MetaMaskError extends Error {
+  code: number | undefined;
+  data: unknown;
+
+  constructor (message: string, code?: number, data?: unknown) {
+    super(message);
+    this.name = 'MetaMaskError';
+    this.code = code;
+    this.data = data;
+  }
+}
 
 /**
  * Detect if MetaMask is installed.
