349ca59e02283cad9295f903c88ab2eefa2c3452
wsdt <kevin.riedl.privat@gmail.com>
2025-10-22 10:11:45 +0200

    fix: outsource snap permissions to permission.json

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -9,7 +9,7 @@ import type {
 
 import { AccountAbstractionKeyring } from './keyring';
 import { logger } from './logger';
-import { InternalMethod, originPermissions } from './permissions';
+import { InternalMethod, getOriginPermissions } from './permissions';
 import { getState } from './stateManagement';
 
 let keyring: AccountAbstractionKeyring;
@@ -24,14 +24,7 @@ async function getKeyring(): Promise<AccountAbstractionKeyring> {
   return keyring;
 }
 
-/**
- * Verify if the caller can call the requested method.
- *
- * @param origin - Caller origin.
- * @param method - Method being called.
- * @returns True if the caller is allowed to call the method, false otherwise.
- */
-function hasPermission(origin: string, method: string): boolean {
+async function hasPermission(origin: string, method: string): Promise<boolean> {
   let baseUrl: string = origin;
   try {
     const { protocol, hostname } = new URL(origin);
@@ -39,7 +32,8 @@ function hasPermission(origin: string, method: string): boolean {
   } catch {
     console.warn('[Snap] Could not extract baseUrl from ', origin);
   }
-  return originPermissions.get(baseUrl)?.includes(method) ?? false;
+  const permissions = await getOriginPermissions();
+  return permissions.get(baseUrl)?.includes(method) ?? false;
 }
 
 export const onRpcRequest: OnRpcRequestHandler = async ({
@@ -51,14 +45,12 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
     JSON.stringify(request, undefined, 2),
   );
 
-  // Check if origin is allowed to call method.
-  if (!hasPermission(origin, request.method)) {
+  if (!(await hasPermission(origin, request.method))) {
     throw new Error(
       `Origin '${origin}' is not allowed to call '${request.method}'`,
     );
   }
 
-  // Handle custom methods.
   switch (request.method) {
     case InternalMethod.SendUserOpBoba:
     case InternalMethod.SendUserOpBobaPM: {
@@ -77,8 +69,6 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
     }
   }
 };
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore TODO: fix types
 export const onKeyringRequest: OnKeyringRequestHandler = async ({
   origin,
   request,
@@ -88,16 +78,11 @@ export const onKeyringRequest: OnKeyringRequestHandler = async ({
     JSON.stringify(request, undefined, 2),
   );
 
-  // Check if origin is allowed to call method.
-  if (!hasPermission(origin, request.method)) {
+  if (!(await hasPermission(origin, request.method))) {
     throw new Error(
       `Origin '${origin}' is not allowed to call '${request.method}'`,
     );
   }
 
-  // Handle keyring methods.
-  // return handleKeyringRequest(await getKeyring(), request);
-  // eslint-disable-next-line
-  // @ts-ignore TODO: fix typeings
   return (await handleKeyringRequest(await getKeyring(), request)) ?? null;
 };
